778f58c516a1d1e27bf35acfd7a844ef2a8cc5ac
Gelir İdaresi <Gelir daresi@DESKTOP-MID53C2>
2023-04-07 17:06:26 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void instaAc()
+        {
+            //driver = webdriver.Chrome(ChromeDriverManager().install());
+            //versiyondan kaynaklı hata veriyor.
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.instagram.com/");
+        }
+        IWebDriver driver = null;
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Thread islem = new Thread(instaAc);
+            islem.Start();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            //mabelmatiz31fan takipçilere girdik
+            driver.Url = ("https://www.instagram.com/mabelmatiz31fan/followers/");
+            driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/ul/li[2]/a"));
+
+            //tüm takipçilerin bulunduğu element (genel class pathini alıp içerisinde butonlu olan ifadeleri seçtik("takipten çıkar")
+            //tüm takiplerin bulunduğu yerdeki butonlar da "takip et" butonu
+            var takip = driver.FindElement(By.TagName("body")).FindElements(By.TagName("ul"));
+
+            //yukarıda iki tane ul geliyo
+            //ikinci ul bizim aradığımız element
+            var butonlar = takip[1].FindElements(By.TagName("button"));
+
+            //tüm takip et butonları içinde gez
+            for(int i=0; i < butonlar.Count; i++)
+            {
+                //her 2 sn de bir bassın
+                Thread.Sleep(2000);
+                butonlar[i].Click();
+            }
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("WindowsFormsApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApp1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f04550e5-0af5-4c51-8074-62e0d2b1c500")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
